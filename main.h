cfe89be847e7d9283327275ea35b59cd0bb9cf7f
“princeaaron” <princeaaron21@gmail.com>
2023-01-24 15:06:21 -0800

    printf main

--- main.h
+++ main.h
@@ -18,6 +18,27 @@ typedef struct prin
 	char flag;
 	char *(*fn)(va_list argu);
 } sir;
+
+/**
+ * struct fmt - Struct op
+ * @fmt: The format
+ * @fn: The function associated
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The format
+ * @fm_t: The function associated
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _strncat(char *buff, const char *format, int n);
 char *fntn(const char symbl, va_list);
